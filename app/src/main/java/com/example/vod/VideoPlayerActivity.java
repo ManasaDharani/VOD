d060aec93c0e519d83496ccd84e714f629723d57
Leo Xu <leo.xu.64@gmail.com>
2020-05-31 17:48:38 -0400

    added video player

--- app/src/main/java/com/example/vod/VideoPlayerActivity.java
+++ app/src/main/java/com/example/vod/VideoPlayerActivity.java
@@ -2,9 +2,21 @@ package com.example.vod;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import java.net.Inet4Address;
 
@@ -21,7 +33,16 @@ public class VideoPlayerActivity extends AppCompatActivity {
         String url = extras.getString("EXTRA_URL");
         TextView titleText = findViewById(R.id.text_title);
         TextView genreText = findViewById(R.id.text_genre);
+        PlayerView playerView = findViewById(R.id.player_view);
         titleText.setText(title);
         genreText.setText(genre);
+        SimpleExoPlayer player = new SimpleExoPlayer.Builder(this).build();
+        playerView.setPlayer(player);
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
+                Util.getUserAgent(this, "yourApplicationName"));
+        MediaSource videoSource =
+                new HlsMediaSource.Factory(dataSourceFactory)
+                        .createMediaSource(Uri.parse(url));
+        player.prepare(videoSource);
     }
 }
